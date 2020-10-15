8cb514c916a258a8d20391a0cae60cad43b0cf71
calebini2 <54257547+calebini2@users.noreply.github.com>
2020-10-15 08:23:23 -0500

    Ejercicio 1 v2

--- Ejercicio_0001.cpp
+++ Ejercicio_0001.cpp
@@ -5,11 +5,17 @@ void  main()
 	int base;
 	int altura;
 	int area;
-
-	base = 10;
-	altura = 20;
+	//Entrada o definicion
+	//Aqui va la carta de amor desesperado(comentario unilinea)
+	printf("Base: ");
+	scanf_s("%i", &base);//Indispensable el '&' 
+	printf("Altura: ");
+	scanf_s("%i", &altura);
+	//Proceso
 	area = base * altura;
-
-	printf("Area del rectangulo: %i", area);
-
+	//Salida
+	printf("Area del rectangulo de %i x %i = %i", base,altura,area);
+	/*Comentario
+	largo de
+	varias lineas*/
 }
