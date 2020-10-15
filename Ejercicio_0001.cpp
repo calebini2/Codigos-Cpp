d416ac52522dec999d8da6e1b07571dd848497f1
calebini2 <54257547+calebini2@users.noreply.github.com>
2020-10-15 08:52:36 -0500

    Ejercicio 1 v5
    Comentarios que ayudan
    
    Decimales

--- Ejercicio_0001.cpp
+++ Ejercicio_0001.cpp
@@ -16,7 +16,8 @@ void  main()
 	//Salida
 	printf("Area del rectangulo de %8.2f x %8.2f = %10.2f", base,altura,area);
 	//%8 pociciones y .2 decimales y 1 pocision la ocupa el punto
-
+	//si solo se pone '%.2' solo dejara el espacio para los decimales, los enteros van a ser los que sean
+	//con %02 rellena con '0' los espacios que no tiene valor
 	/*Comentario
 	largo de
 	varias lineas*/
